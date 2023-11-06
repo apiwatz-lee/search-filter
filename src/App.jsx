60651837912b8fa395dd0196ba4a0e511ab20c0f
Apiwat Lee <apiwatlee@Apiwats-MacBook-Air.local>
2023-11-06 21:28:38 +0700

    filterCountries:project completed

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,15 @@ import axios from "axios";
 
 function App() {
   const [countries, setCountries] = useState([]);
+  const [input, setInput] = useState("");
+  const [keyFilter] = useState(["name", "capital"]); //สร้างเพื่อให้สามารถ search ได่้ 2 properties
 
   const fetchData = async () => {
     try {
       const result = await axios.get("https://restcountries.com/v2/all");
       setCountries(result.data);
     } catch (error) {
-      console.error(`Something went wrong :${error}`);
+      alert(`Something went wrong :${error}`);
     }
   };
 
@@ -18,32 +20,50 @@ function App() {
     fetchData();
   }, []);
 
-  console.log(countries);
+  const filterCountries = (countries) => {
+    return countries.filter((country) => {
+      return keyFilter.some((key) => {
+        return (
+          country[key]?.toString().toLowerCase().indexOf(input.toLowerCase()) >
+          -1
+        );
+      });
+    });
+  };
+
+  const formatNumber = (num) => {
+    return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
+  };
+
   return (
     <div className="w-screen">
-      <main className="border border-black w-[96%] min-h-screen my-0 mx-auto flex flex-col justify-center items-stretch">
+      <main className="w-[96%] min-h-screen my-0 mx-auto flex flex-col justify-center items-center">
         <label htmlFor="search-countries" />
         <input
           type="text"
-          className="border border-slate-700 my-5 p-5 w-full shadow-md rounded-xl"
+          className="border border-slate-400 my-5 p-5 w-full shadow-md rounded-md outline-none"
           placeholder="Search countries or capital"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
         />
-        <section className="border h-auto grid grid-cols-4 gap-10 sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-3">
-          {countries.map((item, index) => {
+        <section className=" h-auto grid grid-cols-4 gap-10 sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-3">
+          {filterCountries(countries).map((item) => {
             return (
               <div
                 className="border px-3 w-[300px] h-[350px] flex flex-col justify-evenly shadow-md"
-                key={index}
+                key={item.name}
               >
                 <img
                   src={item.flag}
                   alt={item.name}
-                  className="object-cover max-w-[250px] border"
+                  className="object-cover max-w-[250px] h-[150px] border"
                 />
                 <h1 className="text-2xl font-bold">{item.name}</h1>
                 <div>
                   population:{" "}
-                  <span className="text-gray-500">{item.population}</span>{" "}
+                  <span className="text-gray-500">
+                    {formatNumber(item.population)}
+                  </span>{" "}
                   people
                 </div>
                 <div>ภูมิภาค: {item.region}</div>
